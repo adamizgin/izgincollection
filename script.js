eedf643a189811d5b703d7122560908595e6037d
adamizgin <88751196+adamizgin@users.noreply.github.com>
2021-10-03 00:47:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ var prePage;
 var product;
 var currentProduct = 1;
 var products = {
-	abun: "The Lord's Prayer in Aramaic according to Syriac tradition. This artwork includes both the traditional frame design from the ancient Syriac Church, but also the modern colours black and gold with a stone texture on a flat surface."
+	abun: "The Lord's Prayer in Aramaic according to Syriac tradition. This artwork includes both the traditional frame design from the ancient Syriac Church, but also the modern colours black and gold with a stone texture on a flat surface.",
+	abun2: "The Lord's Prayer in Aramaic according to Syriac tradition – Shape of a Cross. This artwork includes both the traditional frame design from the ancient Syriac Church, but also the modern colours black and gold with a stone texture on a flat surface."
 }
 var standard = "<h2>Forex board</h2> <hr> <ol> <li>20×30 cm – 60€</li> <li>30×45 cm – 65€</li> <li>40×60 cm – 68€</li> <li>50×75 cm – 85€</li> <li>60×90 cm – 100€</li> <li>80×120 cm – 158€</li> <li>100×150 cm – 230€</li> </ol> <br> <h2>Acrylic glass</h2> <hr> <ol> <li>20×30 cm – 62€</li> <li>30×45 cm – 76€</li> <li>40×60 cm – 95€</li> <li>50×75 cm – 120€</li> <li>60×90 cm – 158€</li> <li>80×120 cm – 220€</li> <li>100×150 cm – 330€</li> </ol> <br> <h2>Acrylic aluminium glass</h2> <hr> <ol> <li>20×30 cm – 70€</li> <li>30×45 cm – 85€</li> <li>40×60 cm – 110€</li> <li>50×75 cm – 158€</li> <li>60×90 cm – 238€</li> <li>80×120 cm – 338€</li> <li>100×150 cm – 468€</li> </ol> <br><br><br> <p>Additions:</p><br> <h2>Wall mount</h2> <hr><br> <h2>Forex board:</h2> <ol> <li>screw bolts – 10€</li> <li>clamps – 10€</li> <li>invisible – 30€</li> </ol> <br> <h2>Acrylic glass:</h2> <ol> <li>crew bolts – 10€</li> <li>clamps – 10€</li> </ol> <br> <h2>Acrylic aluminium glass:</h2> <ol> <li>screw bolts – 10€</li> <li>clamps – 10€</li> <li>invisible – free (0€)</li> </ol>";
 
@@ -15,6 +16,7 @@ $(document).ready(function() {
 	$('.standard').html(standard);
 
 	$('.abun').html(products.abun);
+	$('.abun2').html(products.abun2);
 	$('#ham').click(function() {
 		$('#menu').css({
 			'display': 'block'
@@ -274,8 +276,10 @@ function chooseProduct() {
 	if (product == 1) {
 		$('#product-1').attr('src', 'Syriac_aramaic_lords_prayer_abun_dbashmayo_1.jpg');
 		$('#product-2').attr('src', 'Syriac_aramaic_lords_prayer_abun_dbashmayo_2.jpg');
+		$('.abun').html(products.abun);
 	} else if (product == 2) {
 		$('#product-1').attr('src', 'lords_prayer_aramaic_cross_on_wall_1.jpg');
 		$('#product-2').attr('src', 'lords_prayer_aramaic_cross_on_wall_2.jpg');
+		$('.abun').html(products.abun2);
 	}
 }
