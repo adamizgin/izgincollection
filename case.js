4453d58c769e9a263cffe5c6920f6ab31a9f933e
adamizgin <88751196+adamizgin@users.noreply.github.com>
2021-12-20 00:44:26 +0100

    Update case.js

--- case.js
+++ case.js
@@ -1,4 +1,4 @@
-var iphone_brands = "<option style='display: none;'>Select your phone model</option><option>iPhone 13 Pro Max</option><option>iPhone 13 Pro</option><option>iPhone 13 Pro mini</option><option>iPhone 13</option><option disabled>--</option><option>iPhone 12 Pro Max</option><option>iPhone 12 Pro</option><option>iPhone 12 Pro mini</option><option>iPhone 12</option><option disabled>--</option><option>iPhone SE (2020)</option><option disabled>--</option><option>iPhone 11 Pro Max</option><option>iPhone 11 Pro</option><option>iPhone 11</option><option disabled>--</option><option>iPhone XR</option><option disabled>--</option><option>iPhone XS Max</option><option>iPhone XS</option><option disabled>--</option><option>iPhone 8 Plus</option><option>iPhone 8</option><option disabled>--</option><option>iPhone X</option><option disabled>--</option><option>iPhone 7 Plus</option><option>iPhone 7</option><option disabled>--</option><option>iPhone SE</option><option disabled>--</option><option>iPhone 6s Plus</option><option>iPhone 6s</option><option>iPhone 6 Plus</option><option>iPhone 6</option><option disabled>--</option><option>iPhone 5s</option><option>iPhone 5c</option><option>iPhone 5</option><option disabled>--</option><option>iPhone 4s</option><option>iPhone 4</option>"
+var iphone_brands = "<option style='display: none;'>Select your phone model</option><option>iPhone 13 Pro Max</option><option>iPhone 13 Pro</option><option>iPhone 13</option><option disabled>--</option><option>iPhone 12 Pro Max</option><option>iPhone 12</option><option disabled>--</option><option>iPhone 11 Pro Max</option><option>iPhone 11</option><option disabled>--</option><option>iPhone XR</option><option disabled>--</option><option>iPhone 8 Plus</option><option>iPhone 8</option><option disabled>--</option><option>iPhone X</option><option disabled>--</option><option>iPhone 7 Plus</option><option>iPhone 7</option>"
 var phoneCase = "images/phone_cases/iPhone_160-8x78-1x7-4.png";
 
 $(document).ready(function() {
@@ -29,46 +29,24 @@ $(document).ready(function() {
 
 // change picture of phone case to selected motive
 function motive() {
-	if (document.querySelector('#phone-motive').value == 'Aramean flag: horiz.') {
-		phoneCase = "images/phone_cases/iPhone_160-8x78-1x7-4_Aramean-flag-horizontal.png";
-	} else if (document.querySelector('#phone-motive').value == 'Aramean flag: horiz. with "SURYOYO"') {
-		phoneCase = "images/phone_cases/iPhone_160-8x78-1x7-4_Aramean-flag-horizontal_SURYOYO.png";
-	} else if (document.querySelector('#phone-motive').value == 'Aramean flag: horiz. with "SURYOYO" in Syriac') {
-		phoneCase = "images/phone_cases/iPhone_160-8x78-1x7-4_Aramean-flag-horizontal_SURYOYO_syriac.png";
-	} else if (document.querySelector('#phone-motive').value == 'Aramean flag: horiz. with "SURYAYA"') {
-		phoneCase = "images/phone_cases/iPhone_160-8x78-1x7-4_Aramean-flag-horizontal_SURYAYA.png";
-	} else if (document.querySelector('#phone-motive').value == 'Aramean flag: horiz. with "ARAMEAN"') {
-		phoneCase = "images/phone_cases/iPhone_160-8x78-1x7-4_Aramean-flag-horizontal_ARAMEAN.png";
-	} else if (document.querySelector('#phone-motive').value == 'Aramean flag: horiz. with "ARAMEAN" in Syriac') {
-		phoneCase = "images/phone_cases/iPhone_160-8x78-1x7-4_Aramean-flag-horizontal_ARAMEAN_syriac.png";
-	} else if (document.querySelector('#phone-motive').value == 'Aramean flag: horiz. with "SYRIAC"') {
-		phoneCase = "images/phone_cases/iPhone_160-8x78-1x7-4_Aramean-flag-horizontal_syriac.png";
-	} else if (document.querySelector('#phone-motive').value == 'Aramean flag: vertical') {
-		phoneCase = "images/phone_cases/iPhone_160-8x78-1x7-4_Aramean-flag-vertical.png";
-	} else if (document.querySelector('#phone-motive').value == 'Assyrian flag') {
-		phoneCase = "images/phone_cases/iPhone_160-8x78-1x7-4_Assyrian-flag.png";
-	} else if (document.querySelector('#phone-motive').value == 'Chaldean flag (color)') {
-		phoneCase = "images/phone_cases/iPhone_160-8x78-1x7-4_Chaldean-flag.png";
-	} else if (document.querySelector('#phone-motive').value == 'Chaldean flag (color) with "SURYOYO"') {
-		phoneCase = "images/phone_cases/iPhone_160-8x78-1x7-4_Chaldean-flag_SURYOYO.png";
-	} else if (document.querySelector('#phone-motive').value == 'Chaldean flag (color) with "SURYOYO" in Syriac') {
-		phoneCase = "images/phone_cases/iPhone_160-8x78-1x7-4_Chaldean-flag_SURYOYO_syriac.png";
-	} else if (document.querySelector('#phone-motive').value == 'Chaldean flag (color) with "SURYAYA"') {
-		phoneCase = "images/phone_cases/iPhone_160-8x78-1x7-4_Chaldean-flag_SURYAYA.png";
-	} else if (document.querySelector('#phone-motive').value == 'Chaldean flag (color) with "CHALDEAN"') {
-		phoneCase = "images/phone_cases/iPhone_160-8x78-1x7-4_Chaldean-flag_CHALDEAN.png";
-	} else if (document.querySelector('#phone-motive').value == 'Chaldean flag (color) with "CHALDEAN" in Syriac') {
-		phoneCase = "images/phone_cases/iPhone_160-8x78-1x7-4_Chaldean-flag_CHALDEAN_syriac.png";
+	let model = document.querySelector('#phone-model').value;
+	let phoneMotive = document.querySelector('#phone-motive').value;
+	if (phoneMotive !== 'Select your motive') {
+		if (model == 'iPhone 13 Pro Max' || model == 'iPhone 13 Pro') {
+			phoneCase = "images/phone_cases/iPhone_13_Pro_Max/"+phoneMotive.replace(/ /g, '-')+".png";
+		} else if (model == 'iPhone 13') {
+			phoneCase = "images/phone_cases/iPhone_13/"+phoneMotive.replace(/ /g, '-')+".png";
+		}
+
+
+
+
+
+
+
+
+		setTimeout(function() {
+			$('#case').attr('src', phoneCase);
+		});
 	}
-
-
-
-
-
-
-
-
-	setTimeout(function() {
-		$('#case').attr('src', phoneCase);
-	});
 }
