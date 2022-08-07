ae0650b183ca80c55adb81aec47d053557ab5b7e
adamizgin <88751196+adamizgin@users.noreply.github.com>
2022-08-07 12:25:50 +0200

    Update script.js

--- products/script.js
+++ products/script.js
@@ -1,26 +1,9 @@
 var currentPage = "#home-page";
 var prePage;
 var product;
-var currentProduct = 1;
 var compDir;
 var rabatt = false;
 var clientCountry;
-var price = {
-	standard: 599,
-	semiPremium: 649,
-	case: 219,
-	car: 199,
-	hoodie: 329,
-	polo: 199,
-	rosary: 249,
-	spiga_cross: 199,
-
-	rabatt_standard: 1078,
-	rabatt_semiPremium: 1168,
-	rabatt_case: 149,
-	rabatt_car: 149,
-	rabatt_clothes: 299,
-}
 
 var item;
 
@@ -56,68 +39,6 @@ $(document).ready(function() {
 		$('#rabatt').hide();
 		$('.rabatt-span').hide();
 	}
-
-
-
-
-
-
-
-
-
-
-	// Identify country
-	$.get("https://ipinfo.io", function(response) {
-		clientCountry = response.country;
-	}, 'jsonp');
-
-
-
-
-
-
-
-
-
-	// Set currency
-	setTimeout(function() {
-		if (clientCountry == 'SE') {
-			$('.standard-price').html(price.standard+'kr');
-			$('.semi_premium-price').html(price.semiPremium+'kr');
-			$('.case-price').html(price.case+'kr');
-			$('.car-price').html(price.car+'kr');
-			$('.hoodie-price').html(price.hoodie+'kr');
-			$('.polo-price').html(price.polo+'kr');
-			$('.rosary-price').html(price.rosary+'kr');
-			$('.spiga-cross-price').html(price.spiga_cross+'kr');
-
-			if (rabatt == true) {
-				$('.rabatt-price-standard').html(price.rabatt_standard+'kr');
-				$('.rabatt-price-semi_premium').html(price.rabatt_semiPremium+'kr');
-				$('.rabatt-case-price').html(price.rabatt_case+'kr');
-				$('.car-price').html(price.rabatt_car+'kr');
-				$('.hoodie-price').html(price.hoodie+'kr');
-			}
-		} else {
-			$('.standard-price').html(price.standard/10+'€');
-			$('.semi_premium-price').html(price.semiPremium/10+'€');
-			$('.case-price').html(price.case/10+'€');
-			$('.case-price').html(price.rabatt_case/10+'€');
-			$('.car-price').html(price.car/10+'€');
-			$('.hoodie-price').html(price.hoodie/10+'€');
-			$('.polo-price').html(price.polo/10+'€');
-			$('.rosary-price').html(price.rosary/10+'€');
-			$('.spiga-cross-price').html(price.spiga_cross/10+'€');
-
-			if (rabatt == true) {
-				$('.rabatt-price-standard').html(price.rabatt_standard/10+'€');
-				$('.rabatt-price-semi_premium').html(price.rabatt_semiPremium/10+'€');
-				$('.rabatt-case-price').html(price.rabatt_case/10+'€');
-				$('.car-price').html(price.rabatt_car/10+'€');
-				$('.hoodie-price').html(price.hoodie/10+'€');
-			}
-		}
-	}, 500);
 
 
 
