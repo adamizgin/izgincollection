2c1208cb56d911464d222a8bc609af710c450a19
adamizgin <88751196+adamizgin@users.noreply.github.com>
2022-08-07 12:13:29 +0200

    Update script.js

--- products/script.js
+++ products/script.js
@@ -1,61 +1,286 @@
-$(function() {
-	
-});
+var currentPage = "#home-page";
+var prePage;
+var product;
+var currentProduct = 1;
+var compDir;
+var rabatt = false;
+var clientCountry;
+var price = {
+	standard: 599,
+	semiPremium: 649,
+	case: 219,
+	car: 199,
+	hoodie: 329,
+	polo: 199,
+	rosary: 249,
+	spiga_cross: 199,
 
-function openHeader() {
-	$('.mobile-header-line').eq(1).css({
-		'left': '100%',
-		'opacity': '0'
-	});
-	$('.mobile-header-line').css({
-		'top': '50%'
-	});
-	$('.mobile-header-line').eq(0).css({
-		'transform': 'translate(-50%, -50%) rotate(45deg)'
-	});
-	$('.mobile-header-line').eq(2).css({
-		'transform': 'translate(-50%, -50%) rotate(-45deg)'
-	});
-
-	$('#header-content').show();
-
-	setTimeout(function() {
-		$('#mobile-header-button').click(closeHeader);
-		$('#header-content').css({
-			'opacity': '1',
-			'top': '0'
-		});
-		$('#product-header').css({
-			'height': '321px'
-		});
-	}, 100);
+	rabatt_standard: 1078,
+	rabatt_semiPremium: 1168,
+	rabatt_case: 149,
+	rabatt_car: 149,
+	rabatt_clothes: 299,
 }
 
-function closeHeader() {
-	$('.mobile-header-line').eq(1).css({
-		'left': '50%',
-		'opacity': '1'
+var item;
+
+$(document).ready(function() {
+
+
+
+	// Christmas sale popup
+	if (sessionStorage) {
+		if (sessionStorage.getItem('closed') == 'closed') {
+			$('#rabatt').css({
+				'display': 'none'
+			});
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+	if (rabatt == true) {
+		$('#rabatt').show();
+		$('.orig-price').css({
+			'color': '#0008',
+			'text-decoration': 'line-through'
+		});
+	} else {
+		$('#rabatt').hide();
+		$('.rabatt-span').hide();
+	}
+
+
+
+
+
+
+
+
+
+
+	// Identify country
+	$.get("https://ipinfo.io", function(response) {
+		clientCountry = response.country;
+	}, 'jsonp');
+
+
+
+
+
+
+
+
+
+	// Set currency
+	setTimeout(function() {
+		if (clientCountry == 'SE') {
+			$('.standard-price').html(price.standard+'kr');
+			$('.semi_premium-price').html(price.semiPremium+'kr');
+			$('.case-price').html(price.case+'kr');
+			$('.car-price').html(price.car+'kr');
+			$('.hoodie-price').html(price.hoodie+'kr');
+			$('.polo-price').html(price.polo+'kr');
+			$('.rosary-price').html(price.rosary+'kr');
+			$('.spiga-cross-price').html(price.spiga_cross+'kr');
+
+			if (rabatt == true) {
+				$('.rabatt-price-standard').html(price.rabatt_standard+'kr');
+				$('.rabatt-price-semi_premium').html(price.rabatt_semiPremium+'kr');
+				$('.rabatt-case-price').html(price.rabatt_case+'kr');
+				$('.car-price').html(price.rabatt_car+'kr');
+				$('.hoodie-price').html(price.hoodie+'kr');
+			}
+		} else {
+			$('.standard-price').html(price.standard/10+'€');
+			$('.semi_premium-price').html(price.semiPremium/10+'€');
+			$('.case-price').html(price.case/10+'€');
+			$('.case-price').html(price.rabatt_case/10+'€');
+			$('.car-price').html(price.car/10+'€');
+			$('.hoodie-price').html(price.hoodie/10+'€');
+			$('.polo-price').html(price.polo/10+'€');
+			$('.rosary-price').html(price.rosary/10+'€');
+			$('.spiga-cross-price').html(price.spiga_cross/10+'€');
+
+			if (rabatt == true) {
+				$('.rabatt-price-standard').html(price.rabatt_standard/10+'€');
+				$('.rabatt-price-semi_premium').html(price.rabatt_semiPremium/10+'€');
+				$('.rabatt-case-price').html(price.rabatt_case/10+'€');
+				$('.car-price').html(price.rabatt_car/10+'€');
+				$('.hoodie-price').html(price.hoodie/10+'€');
+			}
+		}
+	}, 500);
+
+
+
+
+
+
+
+
+
+
+	// Loading page hide
+	setTimeout(function() {
+		$('#loading-page').hide();
+	}, 300);
+
+
+
+
+
+
+
+
+
+
+	window.scrollTo(0, 0);
+
+
+
+
+
+
+
+
+
+
+
+
+	$('#ham').click(function() {
+		$('#menu').css({
+			'display': 'block'
+		});
+		setTimeout(function() {
+			$('#menu').css({
+				'left': '0'
+			});
+			$(currentPage).css({
+				'left': '80%'
+			});
+			$('#menu-stop').fadeIn(150);
+			$('html').css({
+				'overflow-y': 'hidden'
+			});
+		});
 	});
-	$('.mobile-header-line').eq(0).css({
-		'top': '25%'
+
+
+
+
+
+
+	$('.header-button').on('click', function() {
+		$hano = $(this);
+			$(this).find('.header-animation').show();
+			$(this).find('.header-animation').css({
+				'height': '0',
+				'transition': 'none'
+			});
+			setTimeout(function() {
+				$hano.find('.header-animation').css({
+					'transition': 'height 250ms ease-in-out'
+				});
+				setTimeout(function() {
+					$hano.find('.header-animation').css({
+						'height': '100%'
+					});
+					setTimeout(function() {
+						$hano.find('.header-animation').fadeOut();
+					}, 450);
+				});
+			});
 	});
-	$('.mobile-header-line').eq(2).css({
-		'top': '75%'
+
+
+
+
+
+
+	$('#rabatt-close').click(function() {
+		setTimeout(function() {
+			$('#rabatt-container').css({
+				'top': '-50px',
+				'opacity': 0
+			});
+			setTimeout(function() {
+				$('#rabatt').fadeOut();
+			}, 500);
+		});
+
+		sessionStorage.setItem('closed', 'closed');
 	});
-	$('.mobile-header-line').eq(0).css({
-		'transform': 'translate(-50%, -50%) rotate(0deg)'
-	});
-	$('.mobile-header-line').eq(2).css({
-		'transform': 'translate(-50%, -50%) rotate(0deg)'
+});
+
+
+
+
+
+
+
+
+
+
+// Functions
+
+function closeMenuButton() {
+	$('#menu').css({
+		'left': '-75%'
 	});
 	setTimeout(function() {
-		$('#mobile-header-button').click(openHeader);
-		$('#header-content').css({
-			'opacity': '0',
-			'top': '-100%'
+		$('#menu').css({
+			'display': 'none'
 		});
-		$('#product-header').css({
-			'height': '60px'
+		$('#menu-stop').fadeOut(150);
+		$('html').css({
+			'overflow-y': 'auto'
 		});
-	}, 100);
+	}, 200);
+}
+
+
+
+
+
+function closeMenu() {
+	$('#menu').css({
+		'left': '-75%'
+	});
+	$(currentPage).css({
+		'left': '50%'
+	});
+	setTimeout(function() {
+		$('#menu').css({
+			'display': 'none'
+		});
+		$('#menu-stop').fadeOut(150);
+		$('html').css({
+			'overflow-y': 'auto'
+		});
+	}, 200);
+}
+
+
+
+
+
+
+
+
+function back() {
+	setTimeout(function() {
+		if (history.back()) {
+			history.back();
+		} else {
+			window.open('https://www.izgincollection.com/index.html', '_self');
+		}
+	});
 }
